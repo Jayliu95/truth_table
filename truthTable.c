aebd007a4fec23cfee517bf5cb75d9549f6b9a4a
Jason Liu <xgl1@rutgers.edu>
2015-08-09 14:38:12 -0400

    update truth table class

--- truthTable.c
+++ truthTable.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "operator_stack.c"
 
 /*functionalities
@@ -31,13 +32,73 @@ struct truth_table{
 	int numCols;
 	elem **input_list;
 };
+/*typedef truth_table as tTable for easier reference*/
+typedef struct truth_table tTable;
 
-struct truth_table* build_Truth_Table(){
-	return NULL;
+#define CHARSET 26
+
+tTable *tTableCreate(int vars){
+	int indexer;
+	tTable *table = (tTable *)malloc(sizeof(tTable));
+	table->numVars = vars;
+	table->numRows = findNumRow(vars);
+	table->numCols = 0; //numVars + operators
+	table->input_list = (elem **) malloc(table->numRows * sizeof(elem*));
+	for(indexer=0; indexer<table->numRows; indexer++){
+		table->input_list[indexer] = (elem *)malloc(vars * sizeof(elem*));
+	}
+	return table;
+}
+
+//helper function that finds numRow based on numVars
+int findNumRow(int raisedTo){
+	int i, retVal;
+	retVal = 1; // if 1 when returned = no variable
+	for(i = 0; i<raisedTo; i++){
+		retVal = retVal * 2;
+	}
+	return retVal;
+}
+
+//helper function that finds the number of variables from user
+int countTotalVar(char *input){
+	int i, j, shouldAdd, numvar;
+	char seen[CHARSET];
+	numvar = 0;
+	for(i=0; i<strlen(input);i++){
+		shouldAdd = 0;
+		if (isalpha(input[i])){
+			for(j=0; j<strlen(seen);j++){
+				if(input[i]==seen[j]){	//have counted the variable already
+					shouldAdd = 1;
+					break;
+				}
+			}
+			//add current char to seen
+			strncat(seen, &input[i], 1);
+			if(shouldAdd==0){
+				numvar++;
+			}
+		}
+	}
+	return numvar;
+}
+
+
+void printTable(tTable *table){
+	int i, j;
+
 }
 
 int main(int argc, char **argv) {
-	oper_stack *oper_list = OSCreate();
-	push(oper_list);
-	pop(oper_list);
+
+	char input[255];
+	int numVar;
+	numVar = 0;
+	printf("enter your operation: \n");
+	gets(input);
+	numVar = countTotalVar(input);	
+	tTable *table = tTableCreate(numVar);
+	printf("table var: %d \t table rows: %d\n", table->numVars,table->numRows);
+	return 0;
 }
